5cadd915c20efad0b4917a99c865dff1bd343aa5
Jordan Mack <jordan.mack@brilliantnotion.com>
2018-09-11 20:32:37 -0700

    Fix additional style issues and reduce font brightness.

--- peepeth-enhancer.js
+++ peepeth-enhancer.js
@@ -7,41 +7,61 @@
         `
             <style>
                 body.dark { background: #141d26; }
+                body.dark { color: #dddddd; }
+                body.dark a { color: #dddddd; }
+                body.dark h1, h2, h3 { color: #dddddd; }
+                body.dark .reveal-overlay .reveal { background: #1b2836; } /* Modal Popups */
+
+                /* Welcome Screen */
+                body.dark #introHeader a { color: #dddddd; }
+                body.dark .introRight { background: #1b2836; }
+                body.dark .introRight a { color: #dddddd; }
+                body.dark .valueDesc { color: gray; }
+
+                /* About / Donations Page */
+                body.dark #donations_page { background: #1b2836 !important; }
+                body.dark #donations_page a { color: #dddddd; }
+
+                body.dark a.peepLink { color: #ff5501; }
+                body.dark a.doubleVerification { color: #d35a38; }
+                body.dark a.acctName { color: gray; }
+                body.dark a.githubLink { color: #ed5933; }
+                body.dark a.twitterLink { color: #ed5933; }
+
                 body.dark #deeplinked-user-tabs { background: #141d26; }
                 body.dark #deeplinked-user-tabs .acctShowBar { background: #243447; }
                 body.dark #deeplinked-user-tabs .tabs-panel.is-active { background: #141d26; }
                 body.dark #deeplinked-user-tabs .acctTab.tabs-title.is-active a { background: transparent; }
-                body.dark { color: white; }
                 body.dark #header { background: #243447; box-shadow: none; }
-                body.dark #headerNavLinks a { color: white; }
-                body.dark #newMessageContent { background: #141d26; color: white; }
-                body.dark .newPostContentFAKE { background: #141d26; color: white; }
+                body.dark #headerNavLinks a { color: #dddddd; }
+                body.dark #newMessageContent { background: #141d26; color: #dddddd; }
+                body.dark .newPostContentFAKE { background: #141d26; color: #dddddd; }
                 body.dark .newPostHolder { background: #1b3448; padding: 5px; }
                 body.dark .postModal.reveal { background: #1b2836; }
                 body.dark .sidebarBox { background: #1b2836; }
                 body.dark #accountSidebar { background: #1b2836; }
                 body.dark #userSidebar { background: transparent; }
-                body.dark #userSidebar #usSidebarRealName { color: white; }
-                body.dark #userSidebar a { color: white; }
-                body.dark #sidebarDetailsHolder #sidebarRealName { color: white; }
+                body.dark #userSidebar #usSidebarRealName { color: #dddddd; }
+                body.dark #userSidebar a { color: #dddddd; }
+                body.dark #sidebarDetailsHolder #sidebarRealName { color: #dddddd; }
                 body.dark .message { background: #1b2836; border-color: #141d26; }
                 body.dark .message:hover { border-left: 2px solid #ff5501; }
-                body.dark .messageHeader .acctRealName { color: white; }
+                body.dark .messageHeader .acctRealName { color: #dddddd; }
                 body.dark #peepethEnhancerSidebar label { color: #b8b8b8; }
-                body.dark a.accountRealName { color: white; }
+                body.dark a.accountRealName { color: #dddddd; }
                 body.dark #mmMessageHolder>.message:hover { background-color: transparent !important; }
                 body.dark #mmMessageHolder>.message:hover .message { background-color: transparent !important; }
-                body.dark #mmReplyToMessageContent { background-color: #141d26; color: white; }
-                body.dark #userMessagesPanel #messages { background-color: #141d26; color: white; }
-                body.dark #userFollowingContent .account { background-color: #1b2836; color: white; }
-                body.dark #userFollowersPanel .account { background-color: #1b2836; color: white; }
-                body.dark #notificationsHolder .notification { background-color: #1b2836; border-color: transparent; color: white; }
-                body.dark #notificationsHolder .notification a { color: white; }
-                body.dark #searchString { background: #141d26; color: white; }
-                body.dark #headerAvatarHolder .dropdown-content { background: #243447; color: white; }
+                body.dark #mmReplyToMessageContent { background-color: #141d26; color: #dddddd; }
+                body.dark #userMessagesPanel #messages { background-color: #141d26; color: #dddddd; }
+                body.dark #userFollowingContent .account { background-color: #1b2836; color: #dddddd; }
+                body.dark #userFollowersPanel .account { background-color: #1b2836; color: #dddddd; }
+                body.dark #notificationsHolder .notification { background-color: #1b2836; border-color: transparent; color: #dddddd; }
+                body.dark #notificationsHolder .notification a { color: #dddddd; }
+                body.dark #searchString { background: #141d26; color: #dddddd; }
+                body.dark #searchStringMain { background: #141d26; color: #dddddd; border: 1px solid gray; }
+                body.dark #accountSearchResults { background: transparent; }
+                body.dark #headerAvatarHolder .dropdown-content { background: #243447; color: #dddddd; }
                 body.dark #headerAvatarHolder .dropdown-content a:hover { background: #1da1f2; }
-                body.dark #batchSignModal { background: #1b2836; }
-                body.dark #batchSignModal a { color: white; }
 
                 #headerImg .show-for-medium img.dark { display: none; }
                 body.dark #headerImg .show-for-medium img { display: none; }
@@ -50,8 +70,10 @@
                 body.dark #headerImg .hide-for-medium img { display: none; }
                 body.dark #headerImg .hide-for-medium img.dark { display: inline-block; }
 
+                /* User Avatar Badges */
                 body.hideBadges .messageAvatarHolder a { display: none !important; }
                 body.hideBadges .messageAvatarHolder a.accountAvatar { display: inline-block !important; }
+                body.hideBadges #usAvatarHolder a { display: none; }
             </style>
         `;
         jQuery(css).appendTo("body");

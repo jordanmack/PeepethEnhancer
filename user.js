18b072e5740db09edc2f0b6b63f201d02fe25efc
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-02-17 14:25:50 -0800

    Switch from RawGit to jsDelivr.

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peepeth Enhancer
 // @namespace    https://github.com/jordanmack/PeepethEnhancer
-// @version      0.3.0
+// @version      0.3.5
 // @description  Peepeth Enhancer is a collection of scripts that add several new features to Peepeth.
 // @author       Jordan Mack (jordanmack.info)
 // @match        https://peepeth.com/*
@@ -11,5 +11,5 @@
 (function() {
     'use strict';
 
-	jQuery.getScript("https://rawgit.com/jordanmack/PeepethEnhancer/live/peepeth-enhancer.js");
+	jQuery.getScript("https://cdn.jsdelivr.net/gh/jordanmack/PeepethEnhancer@live/peepeth-enhancer.js");
 })();

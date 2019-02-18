7f5b004115d26db964a4a61abff30a4933be2b39
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-02-17 21:22:54 -0800

    Bump version.

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peepeth Enhancer
 // @namespace    https://github.com/jordanmack/PeepethEnhancer
-// @version      0.3.5
+// @version      0.3.6
 // @description  Peepeth Enhancer is a collection of scripts that add several new features to Peepeth.
 // @author       Jordan Mack (jordanmack.info)
 // @match        https://peepeth.com/*
